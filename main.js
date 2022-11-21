1dec6e44fc2eb7be9de1a5ffcaac55f592cc2661
sahaprajokta <97291708+sahaprajokta@users.noreply.github.com>
2022-11-21 17:16:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+canvas = document.getElementById('myCanavs');
+ctx = canavs.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+    img_imgTag = new Image();
+    img_imgTag.onload = uploadimg;
+    img_imgTag.src = img_image;
+}
+
+function uploadimg() {
+
+    ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+        alphabetkey();
+        document.getElementById("d1").innerHTML = "You pressed Alphabet key";
+        console.log("alphabetkey");
+    } 
+   else if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+        alphabetkey();
+        document.getElementById("d1").innerHTML = "You pressed Alphabet key";
+        console.log("alphabetkey");
+    }
+    if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+        alphabetkey();
+        document.getElementById("d1").innerHTML = "You pressed Alphabet key";
+        console.log("alphabetkey");
+    }
+    if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+        alphabetkey();
+        document.getElementById("d1").innerHTML = "You pressed Alphabet key";
+        console.log("alphabetkey");
+    }
+    else {
+        otherkey();
+        document.getElementById("d1").innerHTML = "You pressed symbol or other key";
+    }
+}
+
+function alphabetkey() {
+    img = image = "Alpkey.png";
+    add();
+}
+
+function numberkey() {
+    img = image = "numkey.png";
+    add();
+}
+
+function arrowkey() {
+    img = image = "arrkey.png";
+    add();
+}
+
+function specialkey() {
+    img = image = "spkey";
+    add();
+}
+
+function otherkey() {
+    img_image = "otherkey.png";
+    add();
+}
